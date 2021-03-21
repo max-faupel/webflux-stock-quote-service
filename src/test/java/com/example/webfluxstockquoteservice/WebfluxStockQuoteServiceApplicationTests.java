3a10df4e59b1820fa8100e91071eb0decb46a4f4
Max Faupel <max.faupel@gmx.de>
2021-03-21 10:42:36 +0100

    add integration test

--- src/test/java/com/example/webfluxstockquoteservice/WebfluxStockQuoteServiceApplicationTests.java
+++ src/test/java/com/example/webfluxstockquoteservice/WebfluxStockQuoteServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.webfluxstockquoteservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.example.webfluxstockquoteservice.model.Quote;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class WebfluxStockQuoteServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testFetchQuotes() {
+		webTestClient.get().uri("/quotes?size=20").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON).expectBodyList(Quote.class).hasSize(20)
+				.consumeWith(allQuotes -> {
+					assertThat(allQuotes.getResponseBody())
+							.allSatisfy(quote -> assertThat(quote.getPrice()).isPositive());
+							assertThat(allQuotes.getResponseBody()).hasSize(20);
+				});
 	}
 
 }
